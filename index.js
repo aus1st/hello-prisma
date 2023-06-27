85306bf37be589d814057c9786a7180d8b83588d
Ahmed Uddin Siddiqui <aus1st@gmail.com>
2023-06-27 22:33:45 +0500

    create project

---
+++ index.js
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+    //await createData();
+    //console.log('user created')
+    await updateData();
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true
+        },
+    });
+    console.dir(allUsers, { depth: null });
+}
+main().then(async () => {
+    prisma.$disconnect();
+}).catch((err) => {
+    console.log(err);
+    prisma.$disconnect();
+    process.exit(1);
+});
+async function createData() {
+    await prisma.user.create({
+        data: {
+            name: 'Ahmed',
+            email: 'ahmed@outlook.com',
+            posts: {
+                create: { title: 'Hello World' }
+            },
+            profile: {
+                create: { bio: 'I Like trees' }
+            }
+        }
+    });
+}
+async function updateData() {
+    const post = await prisma.post.update({
+        where: { id: 1 },
+        data: { published: true }
+    });
+    console.log(post);
+}
